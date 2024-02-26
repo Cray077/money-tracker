fe125e57a7cc8d7cec0f0cc3124c8aeefdfb9c84
Cray077 <bernando2345@gmail.com>
2024-02-26 19:58:28 +0700

    making database

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-## SQL ##############################
+import sqlite3
+
 
 ## VARIABLES ########################
+conn = sqlite3.connect("main.db")
+cur = conn.cursor()
+
+## SQL ##############################
+def create_table():
+    cur.execute("""
+    CREATE TABLE IF NOT EXIST expense (
+                id  INTEGER PRIMARY KEY,
+                name    TEXT    NOT NULL,
+                amount  INTEGER NOT NULL,
+                description TEXT
+    )""")
+
+def get_expenses():
+    pass
+def add_expense():
+    pass
+
+def remove_expense():
+    pass
+
+def update_expense():
+    pass
 
 ## FUNCTIONS ########################
 
