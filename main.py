a5024ffec63a6c2b665584c20b9944af7506642f
Cray077 <bernando2345@gmail.com>
2025-04-29 10:54:57 +0700

    wrote the sql for update, and added docstring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 
 ## VARIABLES ########################
@@ -7,6 +8,7 @@ cur = conn.cursor()
 
 ## SQL ##############################
 def create_table():
+    
     cur.execute("""
     CREATE TABLE IF NOT EXISTS expense (
                 id  INTEGER PRIMARY KEY,
@@ -18,26 +20,68 @@ def create_table():
     )""")
 
 def get_expenses_byid(expense_id):
+    """get expense by id
+
+    Args:
+        expense_id (int): expense id
+
+    Returns:
+        list: return one item
+    """
     cur.execute("SELECT * FROM expense WHERE id = ?", (expense_id, ))
     return cur.fetchone()
 
 def get_expenses_bycategory(expense_category):
+    """get expenses by category
+
+    Args:
+        expense_category (str): expense category
+
+    Returns:
+        list: all the expense in a list
+    """
     cur.execute("SELECT * FROM expense WHERE category = ?", (expense_category, ))
     return cur.fetchall()
 
-def get_expenses_full():
+def get_expenses():
+    """Get all expenses
+
+    Returns:
+        list: all expenses in a list
+    """
     cur.execute("SELECT * FROM expense")
     return cur.fetchall()
 
-def add_expense(expense_id, expense_name, expense_amount, expense_desc, expense_category, expense_date):
-    cur.execute("INSERT INTO expense (id, name, amount, description, category, date) VALUES (?, ?, ?, ?, ?, ?)", (expense_id, expense_name, expense_amount, expense_desc, expense_category, expense_date))
+def add_expense(expense_id, expense_name, expense_amount, expense_desc, expense_category):
+    """add an expense item
+
+    Args:
+        expense_id (int): the expense id
+        expense_name (str): expense name
+        expense_amount (amount): amount of expense
+        expense_desc (str): expense description (optional)
+        expense_category (str): category ex: primary, secondary, foods, bills
+    """
+    current_date = datetime.datetime.now()
+    cur.execute("INSERT INTO expense (id, name, amount, description, category, date) VALUES (?, ?, ?, ?, ?, ?)", (expense_id, expense_name, expense_amount, expense_desc, expense_category, current_date))
     conn.commit()
 
 def remove_expense(expense_id):
-    cur.execute("")
+    """remove an expense with a given id
 
-def update_expense():
-    pass
+    Args:
+        expense_id (int): expense id
+    """
+    cur.execute("DELETE FROM expense WHERE id = ?", (expense_id, ))
+    conn.commit()
+
+def update_expense(expense_id, expense_name, expense_amount, expense_desc, expense_category):
+
+    current_date = datetime.datetime.now()
+    
+    cur.execute("""UPDATE expense
+                SET id = ?, name = ?, amount = ?, description = ?, category = ?, date = ?
+                WHERE id = ?""", (expense_id, expense_name, expense_amount, expense_desc, expense_category, current_date, expense_id))
 
 ## FUNCTIONS ########################
 
@@ -57,7 +101,7 @@ def main():
     # add_expense(6, "tax", 10000, "", "needs", "11-3-2024")
 
 
-    print_in_list(get_expenses_full())
+    # print_in_list(get_expenses_full())
 
 ## MAINLOOP ##########################
 
